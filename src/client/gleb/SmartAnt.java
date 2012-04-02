22a24d2f1ab3eb34c8fd5ce927c5d5b58e34be5f
hamsters <hamsters@Hamsters>
2012-04-02 14:10:07 +0300

    SmartAnt

---
+++ src/client/gleb/SmartAnt.java
@@ -0,0 +1,14 @@
+package client.gleb;
+
+import client.BaseAnt;
+
+public class SmartAnt extends BaseAnt {
+	private static final long serialVersionUID = -4091734660600450881L;
+
+	
+	@Override
+	protected void setup() {
+		
+		
+	}
+}
